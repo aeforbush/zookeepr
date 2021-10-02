a3feee607729d9370d67c067d16fcb3a3ed1aa30
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-02 11:42:49 -0600

    added zookeeper handler

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const htmlRoutes = require('./routes/htmlRoutes/html');
 const express = require("express");
 // const { animals } = require("./data/animals.json");
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3005;
 // app represents a signle instance of the Express.js server (variable that creates endpoints)
 const app = express();
 
